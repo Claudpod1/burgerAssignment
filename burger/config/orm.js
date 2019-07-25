dbb0d42c765a67040c83ab93e8d451daa3f1426b
Claud Pod <claudiapollinger.cp@gmail.com>
2019-07-25 10:45:23 -0600

    seeds and schema working, update origization of the app, and setting up burgers_controller js file

---
+++ burger/config/orm.js
@@ -0,0 +1,3 @@
+var connection = require("../config/connection.js");
+
+module.exports = orm;
---
+++ burger/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Blue Cheese and Avocado Burger")
+INSERT INTO burgers (burger_name) VALUES ("Veggie Burger")
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger ")
