8cdf681e67dc9c304e9599b3079644ce222d48d0
Claud Pod <claudiapollinger.cp@gmail.com>
2019-07-25 21:55:31 -0600

    got style and got the put method to work

--- burger/controllers/burgers_controller.js
+++ burger/controllers/burgers_controller.js
@@ -29,7 +29,7 @@ router.put("/api/burgers/:id", function(req, res) {
     console.log("Controller put",req.body);
     
     burger.updateBurger({
-      devoured: req.body.devoured
+      devoured: true
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
