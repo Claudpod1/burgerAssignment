2a8edad4d377dbd61d8d4352bcece7c76bb6b0f3
Claud Pod <claudiapollinger.cp@gmail.com>
2019-09-09 16:14:11 -0600

    hopefully fixed the connection.js

--- burger/config/connection.js
+++ burger/config/connection.js
@@ -3,19 +3,24 @@ var mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
-}else {
-
-
+} else {
   var connection = mysql.createConnection({
     host: "localhost",
-    port: 8889,
+    port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
   });
-}
+};
 // Make connection.
-connection.connect()
+connection.connect(function(err){
+  if (err){
+    console.log(err);
+    return;
+
+  }
+  console.log("connected" + connection.threadId);
+});
 
 
 // Export connection for our ORM to use.
